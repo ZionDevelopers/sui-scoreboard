088f454a23f41355be6e060b708cc6f25c63310b
Júlio C. Oliveira <talk@juliocesar.me>
2016-07-13 11:27:32 -0300

    Updated Workshop bat

---
+++ SUIScoreboard-SteamUpdate.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "G:\SteamLibrary\SteamApps\common\GarrysMod\bin"
+gmad.exe create -folder "D:\Github\sui-scoreboard" -out "D:\Github\sui-scoreboard.gma"
+gmpublish update -addon "D:\Github\sui-scoreboard.gma" -id "160121673" -changes "Change it later"
+pause
