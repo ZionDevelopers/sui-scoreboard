6165c1ebb7f6e2c11fbbef2fb1aac546e8f388f3
Júlio C. Oliveira <talk@juliocesar.me>
2023-05-25 14:21:13 -0300

    Version and dates update

--- lua/ev_plugins/sh_sui_scoreboard.lua
+++ lua/ev_plugins/sh_sui_scoreboard.lua
@@ -2,7 +2,7 @@
 
 SUI Scoreboard v2.6 by .Z. Dathus [BR] is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 ----------------------------------------------------------------------------------------------------------------------------
-Copyright (c) 2014 - 2021 .Z. Dathus [BR] <http://www.juliocesar.me> <http://steamcommunity.com/profiles/76561197983103320>
+Copyright (c) 2014 - 2023 .Z. Dathus [BR] <http://www.juliocesar.me> <http://steamcommunity.com/profiles/76561197983103320>
 
 This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US.
@@ -12,7 +12,7 @@ Copyright only on the code that I wrote, my implementation and fixes and etc, Th
 ----------------------------------------------------------------------------------------------------------------------------
 
 $Id$
-Version 2.6.3 - 2021-07-01 06:14 PM(UTC -03:00)
+Version 2.6.4 - 2023-05-25 2:19 PM(UTC -03:00)
 
 ]]--
 
