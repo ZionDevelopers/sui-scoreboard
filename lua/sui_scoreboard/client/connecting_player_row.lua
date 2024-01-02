cef582c6fc130c2ebe08a0dbe30352a5420dab90
Júlio C. Oliveira <talk@juliocesar.me>
2024-01-02 00:11:41 -0300

    Updated copyright year

--- lua/sui_scoreboard/client/connecting_player_row.lua
+++ lua/sui_scoreboard/client/connecting_player_row.lua
@@ -2,7 +2,7 @@
 
 SUI Scoreboard v2.6 by .Z. Dathus [BR] is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 ----------------------------------------------------------------------------------------------------------------------------
-Copyright (c) 2014 - 2023 .Z. Dathus [BR] <http://www.juliocesar.me> <http://steamcommunity.com/profiles/76561197983103320>
+Copyright (c) 2014 - 2024 Dathus [BR] <http://www.juliocesar.me> <http://steamcommunity.com/profiles/76561197983103320>
 
 This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US.
